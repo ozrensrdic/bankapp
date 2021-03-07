f31694a802d8afdfa9f67f8e078ca87c4f3c1ca1
ozrensrdic <ozrensrdic@gmail.com>
2021-03-07 23:46:37 +0100

    Update receiver column name in transaction table

--- migration/20210306130321_create_transactions_table.php
+++ migration/20210306130321_create_transactions_table.php
@@ -12,13 +12,13 @@ class CreateTransactionsTable extends AbstractMigration
                 `id` int(11) NOT NULL AUTO_INCREMENT,
                 `sender_customer_id` int(11) NOT NULL,
                 `sender_branch_id` int(11) NOT NULL,
-                `receiver_sender_id` int(11) NOT NULL,
+                `receiver_customer_id` int(11) NOT NULL,
                 `receiver_branch_id` int(11) NOT NULL,
                 `balance_sent` decimal(11,2) NOT NULL,
                 `created_at` timestamp NOT NULL DEFAULT NOW(),
                 PRIMARY KEY (`id`),
                 FOREIGN KEY (`sender_customer_id`) REFERENCES customers(`id`),
-                FOREIGN KEY (`receiver_sender_id`) REFERENCES customers(`id`),
+                FOREIGN KEY (`receiver_customer_id`) REFERENCES customers(`id`),
                 FOREIGN KEY (`sender_branch_id`) REFERENCES branches(`id`),
                 FOREIGN KEY (`receiver_branch_id`) REFERENCES branches(`id`)
             );'
